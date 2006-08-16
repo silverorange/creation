e5fcfbfede66ca847b7e0a5cccfc02ade9cb3cc9
Nathan Fredrickson <nathan@silverorange.com>
2006-08-16 15:12:56 +0000

    Working Creation package with better parsing thanks to mike.
    git-svn-id: file:///Users/gauthierm/svnrepo/creation/trunk@9706 94490950-a86c-4493-9ccb-530f72933e47

--- Creation/CreationFunction.php
+++ Creation/CreationFunction.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once 'Creation/CreationRelation.php';
+require_once 'Creation/CreationObject.php';
 
-class CreationFunction extends CreationRelation
+class CreationFunction extends CreationObject
 {
 	// {{{ protected function parseName()
 
 	protected function parseName()
 	{
-		$regexp = '/create( or replace)? function ([a-zA-Z0-9]+)/ui';
+		$regexp = '/create(\s+or\s+replace)?\s+function\s+([a-zA-Z0-9_]+)/ui';
 		preg_match($regexp, $this->sql, $matches);
 
 		return $matches[2];
@@ -19,7 +19,17 @@ class CreationFunction extends CreationRelation
 
 	protected function parseDeps()
 	{
-		return array();
+		$regexp = '/returns\s+([a-zA-Z0-9_]+)\(/ui';
+		preg_match_all($regexp, $this->sql, $matches);
+
+		$deps = array();
+		$primative_types = array('varchar', 'char', 'numeric', 'int');
+
+		foreach ($matches[1] as $dep)
+			if (!in_array(strtolower($dep), $primative_types))
+				$deps[] = $dep;
+
+		return $deps;
 	}
 
 	// }}}
--- Creation/CreationView.php
+++ Creation/CreationView.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once 'Creation/CreationRelation.php';
+require_once 'Creation/CreationObject.php';
 
-class CreationView extends CreationRelation
+class CreationView extends CreationObject
 {
 	// {{{ protected function parseName()
 
 	protected function parseName()
 	{
-		$regexp = '/create( or replace)? view ([a-zA-Z0-9]+)/ui';
+		$regexp = '/create(\s+or\s+replace)?\s+view\s+([a-zA-Z0-9_]+)/ui';
 		preg_match($regexp, $this->sql, $matches);
 
 		return $matches[2];
@@ -19,7 +19,7 @@ class CreationView extends CreationRelation
 
 	protected function parseDeps()
 	{
-		$regexp = '/(from|join) ([a-zA-Z0-9]+)/ui';
+		$regexp = '/(from|join)\s+([a-zA-Z0-9_]+)/ui';
 		preg_match_all($regexp, $this->sql, $matches);
 
 		return $matches[2];
