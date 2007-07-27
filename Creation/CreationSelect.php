dfb3d029c0c6ae948c0699791ca95e4e46bdaee1
Nathan Fredrickson <nathan@silverorange.com>
2007-07-27 20:52:16 +0000

    Parse select and insert statements in addition to create statements.
    svn commit r18796

---
+++ Creation/CreationSelect.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'Creation/CreationObject.php';
+
+/**
+ * Parses an SELECT statement
+ *
+ * @package   Creation
+ * @copyright 2007 silverorange
+ */ 
+class CreationSelect extends CreationObject
+{
+	// {{{ protected function parseName()
+
+	protected function parseName()
+	{
+		return md5($this->sql);
+	}
+
+	// }}}
+	// {{{ protected function parseDeps()
+
+	protected function parseDeps()
+	{
+		$regexp = '/select.*from\s+([a-zA-Z0-9_,\w]+)/ui';
+		preg_match($regexp, $this->sql, $matches);
+
+		$deps = array();
+		$tables = explode(',', $matches[1]);
+		foreach ($tables as $table) {
+			$regexp = '/^([a-zA-Z0-9_]+)/ui';
+			if (preg_match($regexp, $table, $matches))
+				$deps[] = $matches[1];
+		}
+
+
+		return $deps;
+	}
+
+	// }}}
+}
+
+?>
