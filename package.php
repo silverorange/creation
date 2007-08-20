488399d5f520358dfb438b41b7a577bde80615c3
Nathan Fredrickson <nathan@silverorange.com>
2007-08-20 15:27:15 +0000

    prepare for release of 0.0.1
    git-svn-id: file:///Users/gauthierm/svnrepo/creation/trunk@19468 94490950-a86c-4493-9ccb-530f72933e47

---
+++ package.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'PEAR/PackageFileManager2.php';
+
+$version = '0.0.1';
+$notes = <<<EOT
+see ChangeLog
+EOT;
+
+$description =<<<EOT
+Parses SQL files, resolves dependencies, and creates objects in database.
+EOT;
+
+$package = new PEAR_PackageFileManager2();
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$result = $package->setOptions(
+	array(
+		'filelistgenerator' => 'svn',
+		'simpleoutput'      => true,
+		'baseinstalldir'    => '/',
+		'packagedirectory'  => './',
+		'dir_roles'         => array(
+			'Creation' => 'php'
+		),
+	)
+);
+
+$package->setPackage('Creation');
+$package->setSummary('A utility to create databases');
+$package->setDescription($description);
+$package->setChannel('pear.silverorange.com');
+$package->setPackageType('php');
+$package->setLicense('LGPL', 'http://www.gnu.org/copyleft/lesser.html');
+
+$package->setReleaseVersion($version);
+$package->setReleaseStability('stable');
+$package->setAPIVersion('0.0.1');
+$package->setAPIStability('stable');
+$package->setNotes($notes);
+
+$package->addIgnore('package.php');
+
+$package->addMaintainer('lead', 'nrf', 'Nathan Fredrickson', 'nathan@silverorange.com');
+$package->addMaintainer('lead', 'gauthierm', 'Mike Gauthier', 'mike@silverorange.com');
+
+$package->setPhpDep('5.1.5');
+$package->setPearinstallerDep('1.4.0');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.2.25');
+$package->generateContents();
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+	$package->writePackageFile();
+} else {
+	$package->debugPackageFile();
+}
+
+?>
