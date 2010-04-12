9a5ae774b72660ec923b1f62f5696ca6dafc8fce
Michael Gauthier <mike@silverorange.com>
2010-04-12 22:47:39 +0000

    Add dependency resolution to stored procedures for MSSQL support.
    svn commit r54362

--- Creation/CreationProcedure.php
+++ Creation/CreationProcedure.php
@@ -25,7 +25,28 @@ class CreationProcedure extends CreationObject
 
 	protected function parseDeps()
 	{
-		return array();
+		$regexp = '/
+			(?:
+				delete\s+from\+s([a-zA-Z0-9_]+)
+				|
+				select\s+.*\s+from\s+([a-zA-Z0-9_]+)
+				|
+				update\s+([a-zA-Z0-9_]+)
+				|
+				exec\s+([a-zA-Z0-9_]+)
+			)/uix';
+
+		preg_match_all($regexp, $this->sql, $matches, PREG_SET_ORDER);
+
+		$deps = array();
+		foreach ($matches as $match) {
+			for ($i = 1; $i <= 4; $i++) {
+				if (isset($match[$i]) && $match[$i]) {
+					$deps[] = $match[$i];
+				}
+			}
+		}
+		return $deps;
 	}
 
 	// }}}
